abc5a4da63c1ba9eb98ab6b17ebe134dc1bd81e6
Vikas <vikassingh.d2e@gmail.com>
2025-10-30 19:05:39 +0530

    update navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,9 +5,9 @@ import { Sidebar, Navbar, Footer } from "./components";
 const App = () => {
   return (
     <div className="relative sm:p-8  p-4 bg-[#eaeaea] dark:bg-[#13131a] min-h-screen flex  ">
-      <div className="sm:flex hidden mr-10 relative">
+      {/* <div className="sm:flex hidden mr-10 relative">
         <Sidebar />
-      </div>
+      </div> */}
       <div className="flex justify-between flex-col w-full gap-10">
         <div className="flex-1 max-sm:w-full  sm:pr-5">
           <Navbar />
